a50ca2e90cb6859cfd2e320c87479db6befcb798
Anna Rodrigues <annagouvea@alunos.utfpr.edu.br>
2023-12-03 21:07:33 -0300

    Ultimos ajustes

--- src/services/buscaRealizadaService.js
+++ src/services/buscaRealizadaService.js
@@ -1,7 +1,6 @@
 const LogDao = require('../dao/logDao')
 
 const salva = (payload) => {
-    // TODO Validar dados antes de salvar
     LogDao.inserir(payload.data, payload.acao, payload.autorId, payload.autorEmail, payload.filter, payload.substring)
 }
 
